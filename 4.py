a8f00affd49733ed804a27e08ee488138698f631
njzjz <njzjz@qq.com>
2016-04-19 15:09:52 +0800

    1

--- 4.py
+++
@@ -1,9 +0,0 @@
-from math import *
-c=0
-for i in range(2,101):
-    for j in range(2,int(sqrt(i)+1)):
-        if i%j==0:break
-    else:
-        print(i,end=' ')
-        c+=1
-        if c%5==0:print()
